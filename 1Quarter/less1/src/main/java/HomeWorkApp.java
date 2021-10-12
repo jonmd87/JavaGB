a488726c22462eadf4ac91536580587814a95ddd
evgen87 <jonmd87@yandex.ru>
2021-10-12 14:53:04 +0300

    added first lesson

---
+++ 1Quarter/less1/src/main/java/HomeWorkApp.java
@@ -0,0 +1,49 @@
+public class HomeWorkApp {
+
+    //Task 1
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    //Task 2
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    //Task 3
+    public static void checkSumSign() {
+        int a = -9;
+        int b = 10;
+        if ((a + b) > 0)
+            System.out.println("Сумма положительная");
+        else
+            System.out.println("Сумма отрицательная");
+    }
+
+    //Task 4
+    public static void printColor() {
+        int value = 101;
+        if (value <= 0)
+            System.out.println("Красный");
+        else if (value > 0 && value <= 100)
+            System.out.println("Желтый");
+        else
+            System.out.println("Зеленый");
+    }
+
+    //Task 5
+    public static void compareNumbers() {
+        int a = 5;
+        int b = 20;
+
+        if (a >= b)
+            System.out.println("a >= b");
+        else
+            System.out.println("a < b");
+    }
+}
