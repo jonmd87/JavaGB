a5b6f68f09de7f94470880d706a50d387ba67069
EvGeN <evgeniigerasimenko@Evgeniis-MacBook-Pro.local>
2021-10-25 12:39:08 +0300

    exercise 3

--- 1Quarter/less3/src/main/java/LessonThree.java
+++ 1Quarter/less3/src/main/java/LessonThree.java
@@ -18,6 +18,17 @@ public class LessonThree {
         for (int i = 0; i < max; i++) {
             arr2[i] = i + 1;
         }
-        System.out.println("after change --> " + Arrays.toString(arr2));
+        System.out.println("exercise 2 --> " + Arrays.toString(arr2));
+
+//ex3
+        System.out.println("Exercise 3");
+        int[] arr3 = new int[] {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        System.out.println("before --> " + Arrays.toString(arr3));
+        for (int i = 0; i < arr3.length; i++) {
+            if (arr3[i] < 6) {
+                arr3[i] *= 2;
+            }
+        }
+        System.out.println("after  --> " + Arrays.toString(arr3));
     }
 }
