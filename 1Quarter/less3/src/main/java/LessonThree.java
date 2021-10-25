422e0fd97bfd66f21c25d5ecb598a92b052b2e98
EvGeN <evgeniigerasimenko@Evgeniis-MacBook-Pro.local>
2021-10-25 13:08:16 +0300

    did exercise 5

--- 1Quarter/less3/src/main/java/LessonThree.java
+++ 1Quarter/less3/src/main/java/LessonThree.java
@@ -10,6 +10,7 @@ public class LessonThree {
             arr1[i] = (arr1[i] == 0 ? 1 : 0);
         }
         System.out.println("after change --> " + Arrays.toString(arr1));
+        System.out.println("");
 
 //ex2
         System.out.println("Exercise 2");
@@ -19,6 +20,7 @@ public class LessonThree {
             arr2[i] = i + 1;
         }
         System.out.println("exercise 2 --> " + Arrays.toString(arr2));
+        System.out.println("");
 
 //ex3
         System.out.println("Exercise 3");
@@ -30,5 +32,36 @@ public class LessonThree {
             }
         }
         System.out.println("after  --> " + Arrays.toString(arr3));
+        System.out.println("");
+
+//ex4
+        System.out.println("Exercise 4");
+        max = 10;
+        int[][] arr4 = new int[max][max];
+        for (int i = 0; i < max; i++) {
+            arr4[i][i] = 1;
+            arr4[i][max - 1 - i] = 1;
+        }
+        for (int i = 0; i < arr4.length; i++) {
+            System.out.print("line [" + i + "] --> ");
+            for (int y = 0; y < arr4[i].length; y++) {
+                System.out.print(arr4[i][y] + " ");
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+
+//ex5
+        System.out.println("Exercise 5");
+        int[] arr5 = createArray(max, max * max - max);
+        System.out.println("createArray(len = 10, initialValue = 90) -return-> " + Arrays.toString(arr5));
+
+        
+    }
+
+    public static int[] createArray(int len, int initialValue) {
+        int arr[] = new int[len];
+        Arrays.fill(arr, initialValue);
+        return (arr);
     }
 }
