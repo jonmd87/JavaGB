70d4872e7d0ecc8ceb1834728c905bb7c1989933
EvGeN <jonmd87@yandex.ru>
2021-11-01 12:49:22 +0300

    less5 done

--- 1Quarter/less5/src/main/java/ru/gb/gerasimenko/Employee.java
+++ 1Quarter/less5/src/main/java/ru/gb/gerasimenko/Employee.java
@@ -3,38 +3,46 @@ package ru.gb.gerasimenko;
 public class Employee {
     private String name;
     private String post;
-    private String telefon;
+    private String telephon;
     private String email;
     private double salary;
     private int age;
 
     public static void main(String[] args) {
         Employee[] arrEmploys = new Employee[5];
-        arrEmploys[0] = new Employee("Ivanov I.I.", "manager","+799988877", "ivanovII@gb.ru", 100000, 50);
-        arrEmploys[1] = new Employee("Petrov P.P.", " finance","+799988877", "ivanovII@gb.ru", 100000, 64);
-        arrEmploys[2] = new Employee("Sidorov S.S.", "security","+799988877", "ivanovII@gb.ru", 100000, 43);
-        arrEmploys[3] = new Employee("Putin V.V.", "direcotor","+799988877", "ivanovII@gb.ru", 100000, 22);
-        arrEmploys[4] = new Employee("Lomonosov M.V.", "scientist","+799988877", "ivanovII@gb.ru", 100000, 30);
+        arrEmploys[0] = new Employee("Ivanov I.I.", "manager","+793918877", "ivanovII@gb.ru", 1000, 50);
+        arrEmploys[1] = new Employee("Petrov P.P.", "finance","+796988174", "petrovPP@gb.ru", 900, 24);
+        arrEmploys[2] = new Employee("Sidorov S.S.", "security","+791682800", "sidorovSS@gb.ru", 1200, 43);
+        arrEmploys[3] = new Employee("Putin V.V.", "direcotor","+700000001", "PutinVV@gb.ru", 100000, 62);
+        arrEmploys[4] = new Employee("Lomonosov M.V.", "scientist","+793280870", "lomonosovMV@gb.ru", 5090, 30);
 
-        for (Employee e: arrEmploys) {
-            e.showData();
+        printListEmployes(arrEmploys, 40);
+    }
+
+//IDE ругался(без static), пришлось добавить
+    public static void printListEmployes(Employee[] arr, int limit) {
+        System.out.printf("%-20s %-15s %-15s %-20s %10s %10s\n", "NAME", "POST", "TELEPHON", "EMAIL", "SALARY", "AGE");
+        for (Employee e: arr) {
+            if (e.age > limit)
+                e.showData();
         }
     }
 
     public void showData() {
-        System.out.println(this.getName());
-        System.out.println(this.getPost());
-        System.out.println(this.getTelefon());
-        System.out.println(this.getEmail());
-        System.out.println(this.getSalary());
-        System.out.println(this.getAge());
+        System.out.printf("%-20s", this.getName());
+        System.out.printf("%-15s", this.getPost());
+        System.out.printf("%-15s", this.getTelephon());
+        System.out.printf("%-20s", this.getEmail());
+        System.out.printf("%15.2f", this.getSalary());
+        System.out.printf("%10d", this.getAge());
+        System.out.println("");
     }
 
-    //Constructor
-    public Employee(String name, String post, String telefon, String email, double salary, int age) {
+//Constructor
+    public Employee(String name, String post, String telephon, String email, double salary, int age) {
         this.name = name;
         this.post = post;
-        this.telefon = telefon;
+        this.telephon = telephon;
         this.email = email;
         this.salary = salary;
         this.age = age;
@@ -50,8 +58,8 @@ public class Employee {
         this.post = post;
     }
 
-    public void setTelefon(String telefon) {
-        this.telefon = telefon;
+    public void setTelephon(String telephon) {
+        this.telephon = telephon;
     }
 
     public void setEmail(String email) {
@@ -77,8 +85,8 @@ public class Employee {
         return post;
     }
 
-    public String getTelefon() {
-        return telefon;
+    public String getTelephon() {
+        return telephon;
     }
 
     public String getEmail() {
