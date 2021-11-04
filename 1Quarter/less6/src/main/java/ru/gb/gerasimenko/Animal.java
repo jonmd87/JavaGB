1896ebcde1ee41936b27e5ea100d53f7edbd8999
jonmd87 <jonmd87@yandex.com>
2021-11-04 19:56:12 +0300

    немного опаздал(забыл запушить)

--- 1Quarter/less6/src/main/java/ru/gb/gerasimenko/Animal.java
+++ 1Quarter/less6/src/main/java/ru/gb/gerasimenko/Animal.java
@@ -1,24 +1,51 @@
 package ru.gb.gerasimenko;
 
-public class Animal
+public abstract class Animal
 {
-    private double  run;
-    private double  sweem;
-    private int     counter;
-
-    public static void main( String[] args ) {
-
-    }
+    private String name;
+    private int run;
+    private int sweem;
+    private static long members = 0;
 
     public Animal(){
-        this.run = 0;
-        this.sweem = 0;
-        counter++;
+        this("null", 0, 0);
     }
 
-    public Animal(double run, double sweem) {
+    public Animal(String name, int run, int sweem) {
+        this.name = name;
         this.run = run;
         this.sweem = sweem;
-        counter++;
+        members++;
+    }
+
+    public String getName() {return this.name;}
+    public int getRun() {return this.run;}
+    public int getSweem() {return this.sweem;}
+
+    public  void run(int distance) {
+        if (distance < 0) {distance = 0;}
+        if (distance > this.run) {
+            System.out.print(this.name + " пробежал " + this.run + " метров, ");
+            System.out.println("оставшиеся " + (distance - this.run) + " метров он прошел.");
+        } else {
+            System.out.println(this.name + " пробежал " + distance + " метров. ");
+        }
+    }
+
+    public void sweem(int distance) {
+        if (this.sweem == 0) {
+            System.out.println(this.name + " не умеет плавать!!!");
+            return ;
+        }
+        if (distance < 0) {distance = 0;}
+        if (distance > this.sweem) {
+            System.out.println(this.name + " не проплывет столько. Не будем мучать животное.");
+        } else {
+            System.out.println(this.name + " проплыл " + distance + " метров.");
+        }
+    }
+
+    public static long getMembers() {
+        return members;
     }
 }
--- 1Quarter/less6/src/main/java/ru/gb/gerasimenko/Dog.java
+++ 1Quarter/less6/src/main/java/ru/gb/gerasimenko/Dog.java
@@ -1,7 +1,14 @@
 package ru.gb.gerasimenko;
 
 public class Dog extends Animal {
-    public static void main(String[] args) {
+    private static long dogsMmbers = 0;
 
+    public static void main(String[] args) {}
+
+    public Dog (String name, int run, int sweem) {
+        super(name, run, sweem);
+        dogsMmbers++;
     }
+
+    public static long getMembers() {return dogsMmbers;}
 }
