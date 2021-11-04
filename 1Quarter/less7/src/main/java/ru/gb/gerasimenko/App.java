bdcdd18914ed03e9e8d7af98d1e877c65bcb91f9
jonmd87 <jonmd87@yandex.com>
2021-11-04 20:23:59 +0300

    created project

---
+++ 1Quarter/less7/src/main/java/ru/gb/gerasimenko/App.java
@@ -0,0 +1,13 @@
+package ru.gb.gerasimenko;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
