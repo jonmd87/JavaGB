06dd3b667865a765c8f1346625d8109a18a55ab1
jonmd87 <jonmd87@yandex.com>
2022-01-12 14:13:59 +0300

    cleaning

--- level_2/Lesson1/src/main/java/ru/gb/gerasimenko/Obstruction.java
+++
@@ -1,7 +0,0 @@
-package ru.gb.gerasimenko;
-
-public abstract class Obstruction {
-    public abstract boolean passObstruction(int value);
-    public abstract String getName();
-    public abstract int getValue();
-}
--- level_2/Lesson1/src/main/java/ru/gb/gerasimenko/Running.java
+++
@@ -1,5 +0,0 @@
-package ru.gb.gerasimenko;
-
-public interface Running {
-    public void run();
-}
